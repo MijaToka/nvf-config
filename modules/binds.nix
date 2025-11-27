da7c548be7debf42f891b51fa50cc4f057ac4e79
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-11-27 22:14:08 +0100

    Change the which key style
    Changes the WhichKey preset to Helix from Modern so that it is less
    invasive and won't interrupt the writting flow when pressing a command
    key.

--- modules/binds.nix
+++ modules/binds.nix
@@ -1,6 +1,11 @@
 {
   vim.binds = {
-    whichKey.enable = true;
+    whichKey = {
+      enable = true;
+      setupOpts = {
+        preset = "helix"; # The style of the which key pop up options are helix classic and rounded
+      };
+    };
     cheatsheet.enable = true;
   };
   vim.globals = {
@@ -17,4 +22,3 @@
     command = { };
   };
 }
-
