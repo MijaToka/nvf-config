7b0c2be323da828af54ad48202ed2f5c45ecafdb
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-12-05 18:04:42 +0100

    Add qmlls to LSP servers and activated lspsaga (shows errors in line)

--- modules/language.nix
+++ modules/language.nix
@@ -5,11 +5,15 @@
     formatOnSave = true;
     lspkind.enable = false;
     lightbulb.enable = true;
-    lspsaga.enable = false;
+    lspsaga.enable = true;
     trouble.enable = true;
     lspSignature.enable = false;
     otter-nvim.enable = true;
     nvim-docs-view.enable = true;
+
+    servers = {
+      qmlls = { };
+    };
   };
 
   vim.languages = {
