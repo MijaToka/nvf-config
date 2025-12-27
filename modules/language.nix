30837f6f9354f8bd1a713d0906236af5d4e46301
MijaToka <mijail.tokarev@ug.uchile.cl>
2025-12-27 12:41:56 -0300

    Add inline diagnostics

--- modules/language.nix
+++ modules/language.nix
@@ -1,31 +1,47 @@
 {
-  vim.lsp = {
-    enable = true;
+  vim = {
+    lsp = {
+      enable = true;
 
-    formatOnSave = true;
-    lspkind.enable = false;
-    lightbulb.enable = true;
-    lspsaga.enable = true;
-    trouble.enable = true;
-    lspSignature.enable = false;
-    otter-nvim.enable = true;
-    nvim-docs-view.enable = true;
+      formatOnSave = true;
+      lspkind.enable = false;
+      lightbulb.enable = true;
+      lspsaga.enable = true;
+      trouble.enable = true;
+      lspSignature.enable = false;
+      otter-nvim.enable = true;
+      nvim-docs-view.enable = true;
 
-    servers = {
-      qmlls = { };
+      servers = {
+        qmlls = {};
+      };
     };
-  };
 
-  vim.languages = {
-    enableTreesitter = true;
+    languages = {
+      enableTreesitter = true;
+      enableExtraDiagnostics = true;
+      enableFormat = true;
 
-    nix.enable = true;
-    markdown.enable = true;
+      nix.enable = true;
+      markdown.enable = true;
 
-    python.enable = true;
-    clang.enable = true;
-    bash.enable = true;
-    css.enable = true;
-    typst.enable = true;
+      python.enable = true;
+      clang.enable = true;
+      bash.enable = true;
+      css.enable = true;
+      typst.enable = true;
+    };
+
+    diagnostics = {
+      enable = true;
+
+      config = {
+        virtual_text = true;
+      };
+
+      nvim-lint = {
+        enable = true;
+      };
+    };
   };
 }
